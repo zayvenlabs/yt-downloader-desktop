26bcd9e9fac9c13c5c203ef0d4718ef68a59649a
Xavier Colombel <colombelxavier7@gmail.com>
2026-06-08 18:35:10 +0200

    Setup Electron with React

---
+++ electron/main.cjs
@@ -0,0 +1,16 @@
+const { app, BrowserWindow } = require("electron");
+const path = require("path");
+
+function createWindow() {
+  const win = new BrowserWindow({
+    width: 1400,
+    height: 900,
+    webPreferences: {
+      preload: path.join(__dirname, "preload.js"),
+    },
+  });
+
+  win.loadURL("http://localhost:5173");
+}
+
+app.whenReady().then(createWindow);
